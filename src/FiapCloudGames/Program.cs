ac29b8309176fad081221c874a80fb5b94a77d62
Maicon Guedes <maicon.ag17@hotmail.com>
2026-04-21 12:47:40 -0300

    Adiciona Serilog para logging estruturado na API

--- src/FiapCloudGames/Program.cs
+++ src/FiapCloudGames/Program.cs
@@ -1,8 +1,17 @@
 using FCG.Api.Extensions;
 using FCG.Application;
 using FCG.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
 
 builder.Services.AddApiPresentation(builder.Configuration);
 builder.Services.AddApplication();
